ac84a4a8e79bc7c0cdf6d0aa6ff1efe8558d034d
Web Dev <tom2kota@gmail.com>
2021-02-23 01:48:41 +0200

    Update text

--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import {Child, ChildAsFC} from "./Child";
 
 const Parent = () => <div>
+    <hr/>
+    <blockquote>Props with TypeScript:</blockquote>
+    <hr/>
     <h1>Parent Component</h1>
     <Child color="green" onClick={() => console.log('clicked')}/>
     <ChildAsFC color="blue" onClick={() => console.log('clicked')}>
         <h3>Children as a prop</h3>
     </ChildAsFC>
+    <hr/>
 </div>
 export default Parent

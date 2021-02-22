e158a4057445461acc7d1f2b2b903aecb08415e2
Web Dev <tom2kota@gmail.com>
2021-02-23 01:38:41 +0200

    Expand Parent and Child components with interface

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 
 interface ChildProps {
-    color: string
+    color: string;
+    onClick: () => void;
 }
 
-export const Child = ({color}: ChildProps) => <div>
+export const Child = ({color, onClick}: ChildProps) => <div>
     <h2>Child Component</h2>
     <p>Color: {color}</p>
+    <button onClick={onClick}>
+        Click me
+    </button>
+</div>
+
+export const ChildAsFC: React.FC<ChildProps> = ({color, onClick, children}) => <div>
+    <h2>Child Functional Component (FC)</h2>
+    <p>Color: {color}</p>
+     {children}
+    <button onClick={onClick}>
+        Click me
+    </button>
 </div>
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import {Child} from "./Child";
+import {Child, ChildAsFC} from "./Child";
 
 const Parent = () => <div>
     <h1>Parent Component</h1>
-    <Child color="green"/>
+    <Child color="green" onClick={() => console.log('clicked')}/>
+    <ChildAsFC color="blue" onClick={() => console.log('clicked')}>
+        <h3>Children as a prop</h3>
+    </ChildAsFC>
 </div>
 export default Parent
